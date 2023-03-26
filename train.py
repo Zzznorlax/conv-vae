37adb0d5f04e51e573ffbb1c113f37e7beef5e33
Zzznorlax <tsuting.yu@gmail.com>
2023-03-27 00:00:36 +0800

    fix(train): fix loss curve oscillating issue in logs

--- train.py
+++ train.py
@@ -66,6 +66,8 @@ def train_vae(opt: Settings):
 
     best_loss = 1e10
     cur_itrs = 0
+    cum_batch = 0
+    train_total_loss = 0
     for epoch in range(opt.EPOCHS):
 
         for batch_idx, img in enumerate(train_loader):
@@ -78,14 +80,16 @@ def train_vae(opt: Settings):
             recon_img, mu, logvar = model(img)
 
             loss = _loss_func(recon_img, img, mu, logvar)
+            train_total_loss += loss.item()
 
             loss.backward()
             optimizer.step()
 
-            logger.info("epoch ({}/{}), batch {}, Training Loss: {}".format(epoch + 1, opt.EPOCHS, batch_idx + 1, loss.item()))
+            # logger.info("epoch ({}/{}), batch {}, Training Loss: {}".format(epoch + 1, opt.EPOCHS, batch_idx + 1, loss.item()))
 
             if (batch_idx + 1) % opt.LOG_INTERVAL == 0:
 
+                val_total_loss = 0
                 model.eval()
                 with torch.no_grad():
                     for val_batch_idx, val_img in enumerate(val_loader):
@@ -95,13 +99,19 @@ def train_vae(opt: Settings):
                         val_recon_img, val_mu, val_logvar = model(val_img)
 
                         val_loss = _loss_func(val_recon_img, val_img, val_mu, val_logvar)
+                        val_total_loss += val_loss.item()
 
-                        logger.info("epoch ({}/{}), batch {}, Validation Loss: {}".format(epoch + 1, opt.EPOCHS, val_batch_idx + 1, val_loss.item()))
+                val_avg_loss = val_total_loss / (opt.LOG_INTERVAL * opt.BATCH_SIZE)
+                train_avg_loss = train_total_loss / (cum_batch * opt.BATCH_SIZE)
+                logger.info("epoch ({}/{}), iteration {}, Validation Loss: {}".format(epoch + 1, opt.EPOCHS, cur_itrs + 1, val_avg_loss))
+                logger.info("epoch ({}/{}), iteration {}, Training Loss: {}".format(epoch + 1, opt.EPOCHS, cur_itrs + 1, train_avg_loss))
 
-                        wandb.log({
-                            "val_loss": val_loss,
-                            "train_loss": loss,
-                        })
+                wandb.log({
+                    "val_loss": val_avg_loss,
+                    "train_loss": train_avg_loss,
+                })
+                train_total_loss = 0
+                cum_batch = 0
 
             if (batch_idx + 1) % opt.CKPT_INTERVAL == 0:
 
@@ -122,7 +132,8 @@ def train_vae(opt: Settings):
                     torch.save(state, ckpt_path)
                     wandb.save(ckpt_path)
 
-        cur_itrs += opt.BATCH_SIZE
+            cum_batch += 1
+            cur_itrs += opt.BATCH_SIZE
 
     logger.info("Training finished")
 
